762c4d8411b7ea8b5daa8f9ce7955c7f62376e26
Ichards <keanurds@gmail.com>
2023-10-20 00:35:58 -0700

    added point snap to point grid, added debug text to help fix

--- point_select.c
+++ point_select.c
@@ -1,7 +1,7 @@
 #include "program_data.h"
 #include "draw_util.h"
 #include "raylib.h"
-
+#include <stdlib.h>
 #include "raymath.h"
 
 void POINT_SELECT_FUNCTION(program_data data) {
@@ -34,6 +34,7 @@ void POINT_SELECT_FUNCTION(program_data data) {
 	Vector3 up = V3((data.save_vector->x == 1) ? 1 : 0, (data.save_vector->y == 1) ? 1 : 0, (data.save_vector->z == 1) ? 1 : 0);
 	Vector3 upinverse = Vector3Subtract(V3(1, 1, 1), *data.grid_up);
 
+
 	Vector3 corner1, corner2;
 	if (upinverse.x == 1) {
 		corner1 = Vector3Add(*data.grid_point, V3(0, 5, -5));
@@ -47,14 +48,6 @@ void POINT_SELECT_FUNCTION(program_data data) {
 		corner1 = Vector3Add(*data.grid_point, V3(5, -5, 0));
 		corner2 = Vector3Add(*data.grid_point, V3(-5, 5, 0));
 	}
-
-/*
-	RayCollision axis_collision = GetRayCollisionQuad(mouse_ray,
-			Vector3Add(*data.grid_point, V3(up.x * 5, up.y * 5, up.z * 5)),
-			Vector3Subtract(*data.grid_point, V3(up.x * 5, up.y * 5, up.z * 5)),
-			corner1,
-			corner2);
-			*/
 	
 	RayCollision axis_collision = GetRayCollisionTriangle(mouse_ray,
 			Vector3Add(*data.grid_point, V3(up.x * 5, up.y * 5, up.z * 5)),
@@ -68,6 +61,20 @@ void POINT_SELECT_FUNCTION(program_data data) {
 			Vector3Subtract(*data.grid_point, V3(up.x * 5, up.y * 5, up.z * 5)));
 	}
 
+	// snap point
+	Vector3 axis_collision_nosnap = V3(0, 0, 0);
+	if (axis_collision.hit) {
+		Vector3 signs = V3(
+			((*data.grid_point).x > 0) - ((*data.grid_point).x < 0),
+			((*data.grid_point).y > 0) - ((*data.grid_point).y < 0),
+			((*data.grid_point).z > 0) - ((*data.grid_point).z < 0)
+		);
+		axis_collision_nosnap = V3((axis_collision.point.x + (0.5 * signs.x)),(axis_collision.point.y + (1.5 * signs.y)),(axis_collision.point.z + (0.5 * signs.z)));
+		axis_collision.point = V3((int)(axis_collision.point.x + (0.5 * signs.x)),(int)(axis_collision.point.y + (1.5 * signs.y)),(int)(axis_collision.point.z + (0.5 * signs.z)));
+	}
+
+
+	char buf[10];
 
     // DRAW MAIN SCREEN
     BeginDrawing();
@@ -76,21 +83,30 @@ void POINT_SELECT_FUNCTION(program_data data) {
 		ClearBackground(data.colors[*(data.program_state)]);
 		DrawRectangle(10, 10, *(data.window_width) - 20, *(data.window_height) - 20, GRAY);
 
+		gcvt(axis_collision_nosnap.x, 1, buf);
+		DrawText(buf, 0, 0, 30, BLACK);
+		gcvt(axis_collision.point.x, 1, buf);
+		DrawText(buf, 180, 0, 30, BLACK);
+
+		gcvt(axis_collision_nosnap.y, 1, buf);
+		DrawText(buf, 0, 30, 30, BLACK);
+		gcvt(axis_collision.point.y, 1, buf);
+		DrawText(buf, 180, 30, 30, BLACK);
+
+		gcvt(axis_collision_nosnap.z, 1, buf);
+		DrawText(buf, 0, 60, 30, BLACK);
+		gcvt(axis_collision.point.z, 1, buf);
+		DrawText(buf, 180, 60, 30, BLACK);
+
+
         BeginMode3D(*data.main_camera);
 
             // DRAW MAIN GRID
             draw_axis(V3(0, 0, 0), 10, BLACK, BLACK, BLACK);
 
  	    draw_grid2(*data.grid_point, 5, 1, *data.grid_up, BLACK);
-		
-		draw_quad(Vector3Add(*data.grid_point, V3(up.x * 5, up.y * 5, up.z * 5)),
-			Vector3Subtract(*data.grid_point, V3(up.x * 5, up.y * 5, up.z * 5)),
-			corner1,
-			corner2,
-			BLUE);
-			
 
-	    DrawSphere(axis_collision.point, 1, BLACK);
+	    DrawSphere(axis_collision.point, 0.5, BLACK);
 
         EndMode3D();
 
